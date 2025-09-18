2add026ae53c756bf5081ce847e3e1b69a4bd5ae
S M Masrafi <smmasrafi000@gmail.com>
2025-09-18 16:45:50 +0600

    Initial commit

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function SinglePostPage() {
+  return (
+    <div>
+      <h1>this is the single post page</h1>
+    </div>
+  );
+}
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,7 @@
+export default function PostsPage() {
+  return (
+    <div>
+      <h1>This is post page</h1>
+    </div>
+  );
+}
